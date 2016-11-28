5c5e323bc46823bae1022f0f2d76e63f0b135775
guoqiang <guoqiang@IBM325-PC0361XM>
2016-11-28 10:11:15 +0800

    添加项目文件。

---
+++ SlbiReport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SlbiReport.Startup))]
+namespace SlbiReport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
